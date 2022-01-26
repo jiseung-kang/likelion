bc2d45bc7faf5fee23e5366d6755b74195d5fca6
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-01-26 16:23:07 +0900

    ✨ daylast : webpack, tdd

---
+++ DAYLAST/TDD/color-button/src/App.test.js
@@ -0,0 +1,10 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
+
+test("버튼이 제대로 작동하고 있나요??", () => {
+	render(<App />);
+	const button = screen.getByRole("button", { name: "change to blue!" });
+	fireEvent.click(button);
+	expect(button).toHaveStyle({ backgroundColor: "blue" });
+	expect(button.textContent).toBe("change to red!");
+});
